d6d9bf8ac52ef04fd80ceaf7c71f17c7b3e684be
ell-hol <yacine.zahidi@gmail.com>
2024-06-01 21:00:55 +0200

    add loft example

---
+++ Cadquery/manually_created/loft.py
@@ -0,0 +1,15 @@
+import cadquery as cq
+
+# Define your sections
+lofted_shape = (
+    cq.Workplane("XY")
+    .circle(10)
+    .workplane(offset=10)
+    .rect(20, 10)
+    .workplane(offset=20)
+    .circle(5)
+    .loft()
+)
+
+
+cq.exporters.export(lofted_shape, "results/loft.stl")
